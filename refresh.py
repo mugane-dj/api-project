2689f2fb5121dac96ac90deae278449cdf234210
Samuel Mugane <muganes25@gmail.com>
2021-06-21 11:29:17 +0300

    spotify refresh token module

---
+++ refresh.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from secrets import refresh_token, base_64
+
+class Refresh_token:
+
+    def __init__(self):
+        self.refresh_token = refresh_token
+        self.base_64 = base_64
+
+    def refresh(self):
+        query = "https://accounts.spotify.com/api/token"
+        response = requests.post(
+            query,
+            data={
+                "grant_type":"refresh_token",
+                "refresh_token":refresh_token
+            },
+            headers={
+                    "Authorization": "Basic " + base_64})
+        response_json = response.json()
+        return response_json['access_token']
+
+
+if __name__=='__main__':
+    r = Refresh_token()
+    r.refresh()
+
+
+
+
