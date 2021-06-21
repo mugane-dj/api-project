d258a0c30217c53d7a5c594abb5427c2038e85d2
roguecode25 <muganes25@gmail.com>
2021-06-21 11:16:45 +0300

    final test

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Playlist:
 
     def get_youtube_client(self):
         print("Fetching client...")
+
         os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
         api_service_name = "youtube"
         api_version = "v3"
@@ -28,7 +29,9 @@ class Playlist:
         credentials = flow.run_local_server(port=8080, prompt="consent")
         youtube_client = googleapiclient.discovery.build(
             api_service_name, api_version, credentials=credentials)
+
         print("client fetching successful!")
+
         return youtube_client
 
     def get_playlist_items(self):
@@ -36,44 +39,46 @@ class Playlist:
         request = youtube_client.playlistItems().list(
             part="snippet",
             playlistId=self.youtube_playlist_id,
-            #maxResults=30
+            maxResults=15
         )
+
         response = request.execute()
-        print(response)
+
         print("Looping through playlist...")
+
         for item in response["items"]:
+            video_title = item["snippet"]["title"]
             video_Id = item["snippet"]["resourceId"]["videoId"]
             youtube_url = "https://www.youtube.com/watch?v={}".format(video_Id)
             video = youtube_dl.YoutubeDL({}).extract_info(
                 youtube_url, download=False, force_generic_extractor=True)
             creator = video["title"]
             artist = creator.split("-")[0]   
-            print(artist)
             track = video["title"]
             song_name = track.split("-")[1].split("| A COLORS SHOW")[0]
-            print(song_name) 
-        for item in response["items"]:
-            video_title = item["snippet"]["title"]
-        if song_name is not None and artist is not None:
+
             self.song_info[video_title] = {
                 "youtube_url": youtube_url,
                 "song_name": song_name,
                 "artist": artist,
                 "spotify_uri": self.get_spotify_uri(song_name, artist)
             }
-        print(self.song_info)
 
     def create_playlist(self):
         """Create A Playlist in Spotify"""
         self.refresh()
+
         request_body = json.dumps({
             "name": "Living Color",
-            "description": "All songs in colors youtube playlist",
+            "description": "Songs in colors youtube playlist",
             "public": False
         })
-        print("Creating spotify playlist")
+
+        print("Creating spotify playlist...")
+
         query = "https://api.spotify.com/v1/users/{}/playlists".format(
             spotify_user_id)
+
         response = requests.post(
             query,
             data=request_body,
@@ -87,10 +92,12 @@ class Playlist:
 
     def get_spotify_uri(self, song_name, artist):
         self.refresh()
+
         query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
             song_name,
             artist
         )
+
         response = requests.get(
             query,
             headers={
@@ -98,19 +105,24 @@ class Playlist:
                 "Authorization": "Bearer {}".format(self.spotify_token)
             })
         response_json = response.json()
-        print(response_json["tracks"])
         songs = response_json["tracks"]["items"]
         uri = songs[0]["uri"]
         return uri 
+
     def add_songs_to_spotify_playlist(self):
         self.get_playlist_items()
+
         uris = [info["spotify_uri"]for song, info in self.song_info.items()]
+
         playlist_id = self.create_playlist()
+
         request_data = json.dumps(uris)
 
         query = "https://api.spotify.com/v1/playlists/{}/tracks".format(
             playlist_id)
+
         print("Finalizing...")
+
         response = requests.post(
             query,
             data=request_data,
